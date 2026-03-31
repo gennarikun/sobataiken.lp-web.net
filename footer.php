3c7e71f5ecba960bc70a249d88e4021034f6f448
gennarikun <gennarikun@gmail.com>
2026-04-01 06:51:09 +0900

    Initialize clean static site

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer class="section siteFooter">
+			<div class="footerMenu">
+			<div class="container">
+				<nav class="menu-a-container"><ul id="menu-a-1" class="menu nav"><li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><a href="/contact/">お申込み・アクセス</a></li>
+<li id="menu-item-1716" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1716"><a href="/voice/">お客様の声・感想</a></li>
+<li id="menu-item-2380" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-2380"><a href="/menu/">メニュー・料金</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/dantai/">団体ランチ</a></li>
+<li id="menu-item-3358" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3358"><a href="/birthday/">誕生日お祝いプラン</a></li>
+<li id="menu-item-2132" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2132"><a href="/suridane/">お土産すりだね販売</a></li>
+<li id="menu-item-1661" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1661"><a href="/gallery/">体験ギャラリー</a></li>
+<li id="menu-item-2560" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2560"><a href="/photo-service/">写真撮影サービス</a></li>
+</ul></nav>			</div>
+		</div>
+			
+	
+	<div class="container sectionBox copySection text-center">
+			<p>Copyright © そば打ち＆染物体験教室｜山梨県河口湖・山中湖・忍野八海の本格そば作り｜プライベート・家族連れに人気 All Rights Reserved.</p><p>Powered by <a href="https://wordpress.org/">WordPress</a> &amp; <a href="https://lightning.nagoya/ja/" target="_blank" title="Free WordPress Theme Lightning"> Lightning Theme</a> by Vektor,Inc. technology.</p>	</div>
+</footer>
+<div id="vk-mobile-nav-menu-btn" class="vk-mobile-nav-menu-btn">MENU</div><div class="vk-mobile-nav vk-mobile-nav-drop-in" id="vk-mobile-nav"><nav class="vk-mobile-nav-menu-outer" role="navigation"><ul id="menu-a-2" class="vk-menu-acc menu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-31"><a href="/contact/">お申込み・アクセス</a>
+<ul class="sub-menu">
+	<li id="menu-item-1670" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1670"><a href="/rule/">注意事項</a></li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1716"><a href="/voice/">お客様の声・感想</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2380"><a href="/menu/">メニュー・料金</a>
+<ul class="sub-menu">
+	<li id="menu-item-3347" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3347"><a href="/" aria-current="page">山梨県プレミアム体験プラン</a></li>
+	<li id="menu-item-2378" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2378"><a href="/omiyage/">お食事なし麺作り体験</a></li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/dantai/">団体ランチ</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3358"><a href="/birthday/">誕生日お祝いプラン</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2132"><a href="/suridane/">お土産すりだね販売</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1661"><a href="/gallery/">体験ギャラリー</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2560"><a href="/photo-service/">写真撮影サービス</a></li>
+</ul></nav></div>
---
+++ header.php
@@ -0,0 +1,31 @@
+<header class="siteHeader">
+		<div class="container siteHeadContainer">
+		<div class="navbar-header">
+						<p class="navbar-brand siteHeader_logo">
+			<a href="/">
+				<span><img src="/assets/389476.jpg" alt="そば打ち＆染物体験教室｜山梨県河口湖・山中湖・忍野八海の本格そば作り｜プライベート・家族連れに人気"></span>
+			</a>
+			</p>
+					</div>
+
+					<div id="gMenu_outer" class="gMenu_outer">
+				<nav class="menu-a-container"><ul id="menu-a" class="menu gMenu vk-menu-acc"><li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children"><a href="/contact/"><strong class="gMenu_name">お申込み・アクセス</strong></a>
+<ul class="sub-menu">
+	<li id="menu-item-1670" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/rule/">注意事項</a></li>
+</ul>
+</li>
+<li id="menu-item-1716" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/voice/"><strong class="gMenu_name">お客様の声・感想</strong></a></li>
+<li id="menu-item-2380" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children"><a href="/menu/"><strong class="gMenu_name">メニュー・料金</strong></a>
+<ul class="sub-menu">
+	<li id="menu-item-3347" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/">山梨県プレミアム体験プラン</a></li>
+	<li id="menu-item-2378" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/omiyage/">お食事なし麺作り体験</a></li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/dantai/"><strong class="gMenu_name">団体ランチ</strong></a></li>
+<li id="menu-item-3358" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/birthday/"><strong class="gMenu_name">誕生日お祝いプラン</strong></a></li>
+<li id="menu-item-2132" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/suridane/"><strong class="gMenu_name">お土産すりだね販売</strong></a></li>
+<li id="menu-item-1661" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/gallery/"><strong class="gMenu_name">体験ギャラリー</strong></a></li>
+<li id="menu-item-2560" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="/photo-service/"><strong class="gMenu_name">写真撮影サービス</strong></a></li>
+</ul></nav>			</div>
+			</div>
+	</header>
